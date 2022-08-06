d391a6518592c2be106d4ead3a4e250b6c987e2c
Tran Hung Trong <tranhungtrong123@gmail.com>
2022-08-07 00:13:25 +0700

    Hoàn thành DashBoard

--- src/app/dasboard/dasboard.component.ts
+++ src/app/dasboard/dasboard.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dasboard.component.css']
 })
 export class DasboardComponent implements OnInit {
-
+  controls = [
+    {name: 'Toà Nhà', path: '/building'},
+    {name: 'Phòng', path: '/room'},
+    {name: 'Người Thuê', path: '/tenant'},
+    {name: 'Nhân Viên', path: '/employee'},
+    {name: 'Hợp Đồng', path: '/contract'},
+    {name: 'Hoá đơn', path: '/invoice'},
+    {name: 'Chí phí bảo trì', path: '/maintenance'},
+    {name: 'Thông báo', path: '/announcement'},
+    {name: 'Dịch Vụ', path: '/service'},
+  ]
   constructor() { }
 
   ngOnInit(): void {
